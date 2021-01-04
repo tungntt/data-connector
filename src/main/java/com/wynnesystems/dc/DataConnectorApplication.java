4674b8f8cd139ebe8cf944a7b2dca13dcd3d5445
Tung Nguyen <tung.nguyen@digicommercegroup.com>
2021-01-04 23:07:05 +0700

    Initial commit

---
+++ src/main/java/com/wynnesystems/dc/DataConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.wynnesystems.dc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataConnectorApplication.class, args);
+    }
+
+}
